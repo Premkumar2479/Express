7ac73ea28b206308a5870bc9cabaf52e1bfa4404
Premkumar2479 <pk2335301@gmail.com>
2025-10-04 15:55:49 +0530

    some text

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ let port = 3000;
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
 });
+
+app.use((req,res) =>{
+   // console.log(req);
+    console.log("request recevied");
+    res.send({
+        name:"apple",
+        color:"red",
+    });
+});
